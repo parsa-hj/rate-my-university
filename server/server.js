1fc8dd8d3604d9f63009674a08cc58eaf884eb09
Parsa Hajiha <hajihaparsa2004@gmail.com>
2024-10-22 15:01:08 -0500

    sql db connected to express

--- server/server.js
+++ server/server.js
@@ -1,23 +1,36 @@
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, "public")))
-app.use(cors())
-app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
 
-const port = 5000
+const port = 5000;
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "rate-my-university"
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Rate My University API");
+});
+
+app.get("/universities", (req, res) => {
+  const q = "SELECT * FROM universities";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 app.listen(port, () => {
-    console.log('listening')
-})
+  console.log("Connect to backend.");
+});
